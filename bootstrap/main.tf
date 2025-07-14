e6706ad5593ed370cc8dd3e69ffaeab5137ba4ad
Rauni Ribeiro <raunirr98@gmail.com>
2025-07-14 13:57:42 +0200

    fix/terraform.yml (github actions) - removing the validate step

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -9,7 +9,7 @@ resource "aws_dynamodb_table" "lambda_db_table" {
     type = "S"
   }
 
-  
+
 }
 
 #creating a DynamoDB table item to be used by the Lambda function (name)
@@ -18,8 +18,8 @@ resource "aws_dynamodb_table_item" "admin_credentials" {
   hash_key   = "id"
 
   item = jsonencode({
-    id = {S = var.username_app}
-    password = {S = var.password_app}
+    id       = { S = var.username_app }
+    password = { S = var.password_app }
   })
 }
 
@@ -29,7 +29,7 @@ resource "aws_dynamodb_table_item" "admin_credentials" {
 resource "aws_iam_policy" "lambda_iam_policy" {
   name        = "lambda_exec_policy"
   description = "IAM policy for Lambda execution role"
-  policy      = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
@@ -38,8 +38,8 @@ resource "aws_iam_policy" "lambda_iam_policy" {
           "dynamodb:GetItem",
           "dynamodb:UpdateItem", #this one is optional
           "dynamodb:DeleteItem", #this one is optional
-          "dynamodb:Scan", #this one is optional
-          "dynamodb:Query", #this one is optional
+          "dynamodb:Scan",       #this one is optional
+          "dynamodb:Query",      #this one is optional
           "logs:CreateLogGroup", #this one is optional
         ],
         Effect   = "Allow",
@@ -68,20 +68,20 @@ resource "aws_iam_role" "lambda_iam_role" {
 
 #assigning the lambda policy to the IAM role
 resource "aws_iam_role_policy_attachment" "policy-role-attachment" {
-  role = aws_iam_role.lambda_iam_role.name
+  role       = aws_iam_role.lambda_iam_role.name
   policy_arn = aws_iam_policy.lambda_iam_policy.arn
 }
 
 #creating the lambda function that will be invoked in the API Gateway
 resource "aws_lambda_function" "API-app" {
   function_name = "API-app"
-    role = aws_iam_role.lambda_iam_role.arn #parsing the role from the IAM role created above
-    handler = "handler.lambda_handler"
-    runtime = "python3.12"
+  role          = aws_iam_role.lambda_iam_role.arn #parsing the role from the IAM role created above
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.12"
 
-    filename = "function.zip" #the zip file containing the lambda function code
+  filename = "function.zip" #the zip file containing the lambda function code
 
-    source_code_hash = filebase64sha256("function.zip") #hash of the zip file to ensure the code is up to date
+  source_code_hash = filebase64sha256("function.zip") #hash of the zip file to ensure the code is up to date
 
 }
 
@@ -98,10 +98,10 @@ resource "aws_api_gateway_rest_api" "login_app_api" {
       "/login" = {
         post = {
           x-amazon-apigateway-integration = {
-            type                 = "AWS_PROXY"
-            httpMethod           = "POST"
-            uri                  = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.API-app.arn}/invocations"
-            IntegrationHTTPMethod = "POST"          
+            type                  = "AWS_PROXY"
+            httpMethod            = "POST"
+            uri                   = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.API-app.arn}/invocations"
+            IntegrationHTTPMethod = "POST"
           }
         }
       }
@@ -118,7 +118,7 @@ resource "aws_api_gateway_deployment" "api-deployment" {
 #creating a stage for the API Gateway
 resource "aws_api_gateway_stage" "dev-stage" {
   stage_name    = "dev"
-  rest_api_id  = aws_api_gateway_rest_api.login_app_api.id
+  rest_api_id   = aws_api_gateway_rest_api.login_app_api.id
   deployment_id = aws_api_gateway_deployment.api-deployment.id
 }
 
